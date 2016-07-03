b1becb8ddd94173bbc494a2cc19e520309c3a17e
margold <irina.gossmann@gmail.com>
2016-07-03 18:48:10 -0400

    Use page actions and declarative content rules
    Switch from browser actions to page actions, and use declarative content
    rules to do away with the content script altogether. This doesn't give
    the required functionality though since injecting a script via
    declarative content (RequestContentScript) is still experimental (?),
    and activeTab permissions (that allow us to inject a script on the
    active tab) are enabled by the user clicking on the page action ican.
    
    So, dead end, but left here for reference.

--- background.js
+++ background.js
@@ -1,7 +1,33 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+// make Hot Network Questions visible and disable page action on click on extension image
+//FIXME toggle instead: https://developer.chrome.com/extensions/pageAction
+chrome.pageAction.onClicked.addListener(function(tab) {
 	// Send a message to the active tab
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		var activeTab = tabs[0];
-		chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+	console.log("Page Action clicked");
+	// security considerations: https://developer.chrome.com/extensions/content_scripts#pi
+	chrome.tabs.executeScript({
+		code: 'document.getElementById("hot-network-questions").style.visibility = "hidden";'
 	});
+	// FIXME what is my state? chrome.pageAction doesn't seem to have one, unless I set it via title
+	chrome.pageAction.hide(tab.id); // https://developer.chrome.com/extensions/tabs#type-Tab
+});
+
+// rules
+// https://developer.chrome.com/extensions/declarativeContent#type-PageStateMatcher
+var se_rule = {
+	id: 'stackexchange',
+	conditions: [
+		new chrome.declarativeContent.PageStateMatcher({
+			pageUrl: { hostSuffix: '.stackexchange.com', schemes: ['http', 'https'] } //add css; "*://stackoverflow.com/*", "*://serverfault.com/*"
+		})
+	],
+	actions: [ new chrome.declarativeContent.ShowPageAction() ]
+};
+
+// added rules are persistent across browser restarts, so only register them on installation/update
+chrome.runtime.onInstalled.addListener(function() {
+	// clear installed rules
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		// add rules
+		chrome.declarativeContent.onPageChanged.addRules([se_rule]);
+    });
 });
